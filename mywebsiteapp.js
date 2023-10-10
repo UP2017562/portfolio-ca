b671c45f3abff9c7c0961d1dadf9d47601d72c50
Cameron Alexander <91853736+UP2017562@users.noreply.github.com>
2023-10-10 11:17:10 +0200

    added a database, only blogs and users at the moment

--- mywebsiteapp.js
+++ mywebsiteapp.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
-
+const sqlite3 = require('sqlite3')
 const session = require('express-session')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
@@ -55,6 +55,62 @@ app.use(session({
   "resave": false,
   "secret": "This123Is@Another#456GreatSecret678%Sentence"
 }));
+
+//-------------------------------
+//  DATABASE CREATION
+//-------------------------------
+
+const db = new sqlite3.Database('portfolio-ca3.db')
+db.run("CREATE TABLE User (user_id INTEGER PRIMARY KEY, user_name TEXT NOT NULL, pass_word TEXT NOT NULL, first_name TEXT NOT NULL, last_name TEXT NOT NULL)", (error) => {
+	if (error) {
+      // tests error: display error
+      console.log("ERROR: ", error)
+    } else {
+      // tests error: no error, the table has been created
+      console.log("---> Table projects created!")
+  
+      const user=[
+        { "id":"1", "username":"cameron_alex", "password":"12345", "fname": "Cameron", "lname": "Alexander"},
+        { "id":"2", "username":"jerome_l", "password":"12345", "fname": "Jerome", "lname": "Landre"}
+      ]
+      // inserts users
+      user.forEach( (oneUser) => {
+        db.run("INSERT INTO User (user_id, user_name, pass_word, first_name, last_name) VALUES (?, ?, ?, ?, ?)", [oneUser.id, oneUser.username, oneUser.password, oneUser.fname, oneUser.lname], (error) => {
+          if (error) {
+            console.log("ERROR: ", error)
+          } else {
+            console.log("Line added into the Users table!")
+          }
+        })
+      })
+    }
+  })
+
+db.run("CREATE TABLE Blogs(blogs_id INTEGER PRIMARY KEY, user_id INTEGER, blog_title TEXT, blog_image TEXT, blog_desc TEXT, blog_date TEXT, FOREIGN KEY (user_id) REFERENCES User (user_id))", (error) => {
+	if (error) {
+      // tests error: display error
+      console.log("ERROR: ", error)
+    } else {
+      // tests error: no error, the table has been created
+      console.log("---> Table blogs created!")
+  
+      const blogs=[
+        { "id":"1", "userid":"1", "title":"Sweden", "image": "", "desc": "Currently I am in Sweden enjoying an exchange year!!! Meeting loads of new friends!", "date": "19/08/2023"},
+        { "id":"2", "userid":"1", "title":"Second Year Uni", "image": "", "desc": "I have just completed my second year of University, now onto the last year! or hopefully a year in sweden :)", "date": "02/06/2023"}
+      ]
+      // inserts blogs
+      blogs.forEach( (oneBlogs) => {
+        db.run("INSERT INTO Blogs (blogs_id, user_id, blog_title, blog_image, blog_desc, blog_date) VALUES (?, ?, ?, ?, ?, ?)", [oneBlogs.id, oneBlogs.user, oneBlogs.title, oneBlogs.image, oneBlogs.desc, oneBlogs.date], (error) => {
+          if (error) {
+            console.log("ERROR: ", error)
+          } else {
+            console.log("Line added into the Blogs table!")
+          }
+        })
+      })
+    }
+  })
+
 
 //-------------------------------
 //  LOG IN APP
