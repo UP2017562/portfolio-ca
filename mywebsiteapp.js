f9b74836f5ef9250a44d0473acffe4332898e84e
Cameron Alexander <91853736+UP2017562@users.noreply.github.com>
2023-10-11 16:19:03 +0200

    Adding the new blog page, function working properly

--- mywebsiteapp.js
+++ mywebsiteapp.js
@@ -147,6 +147,127 @@ app.get('/blogs', function(req,res){
   })
 })
 
+//--------------------
+// CREATES NEW BLOG
+//--------------------
+
+// SENDS THE FORM FOR A NEW PROJECT
+app.get('/blogs/new', (req, res) => {
+  if (req.session.isLoggedIn==true && req.session.isAdmin==true) {
+    const model = {
+      style: "blogs.css",
+      isLoggedIn: req.session.isLoggedIn,
+      name: req.session.name,
+      isAdmin: req.session.isAdmin,
+    }
+    res.render("newblogs.handlebars", model)
+  } else {
+    res.redirect('/login')
+  }
+})
+
+// CREATES NEW PROJECT
+app.post('/blogs/new', (req, res) => {
+  const newp = [
+    req.body.blogtitle, req.body.blogdate, req.body.blogdesc, req.body.bloguser, req.body.blogimg,
+  ]
+  if (req.session.isLoggedIn==true && req.session.isAdmin==true) {
+    db.run("INSERT INTO blogs (blog_title, blog_date, blog_desc, blogs_uid, blog_image) VALUES (?, ?, ?, ?, ?)", newp, (error) => {
+      if (error) {
+        console.log("ERROR: ", error)
+      } else {
+        console.log("Line added into the Blogs table!")
+      }
+      res.redirect('/blogs')
+    })
+  } else {
+    res.redirect('/login')
+  }
+})
+
+//-----------------
+// MODIFY A BLOG
+//-----------------
+
+//SENDS TEH FORM TO MODIFY A PROJECT
+app.get('/blogs/update/:id', (req, res) => {
+  const id = req.params.id
+  db.get("SELECT * FROM projects WHERE pid=?", [id], function (error, theProject) {
+    if (error) {
+      console.log("ERROR: ", error)
+      const model = { dbError: true, theError: error,
+        project: {},
+        isLoggedIn: req.session.isLoggedIn,
+        name: req.session.name,
+        isAdmin: req.session.isAdmin,
+      }
+      res.render("modifyproject.handlebars", model)
+    } else {
+      const model = { dbError: false, theError: "",
+        project: theProject,
+        isLoggedIn: req.session.isLoggedIn,
+        name: req.session.name,
+        isAdmin: req.session.isAdmin,
+        helpers: {
+          theTypeR(value) { return value == "Research"; },
+          theTypeT(value) { return value == "Teaching"; },
+          theTypeO(value) { return value == "Other"; },
+        }
+      }
+      res.render("modifyproject.handlebars", model)
+    }
+  })
+})
+
+app.post('/projects/update/:id', (req, res) => {
+  const id = req.params.id // gets the id from the dynamic parameter in the route
+  const newp = [
+    req.body.projname, req.body.projyear, req.body.projdesc, req.body.projtype, req.body.projimg, id
+  ]
+  if (req.session.isLoggedIn==true && req.session.isAdmin==true) {
+    db.run("UPDATE projects SET pname=?, pyear=?, pdesc=?, ptype=?, pimgURL=? WHERE pid=?", newp, (error) => {
+      if (error) {
+        console.log("ERROR: ", error)
+      } else {
+        console.log("Project Updated!")
+      }
+      res.redirect('/projects')
+    })
+  } else {
+    res.redirect('/login')
+  }
+})
+
+//------------------
+// DELETES A PROJECT
+//------------------
+
+app.get('/projects/delete/:id', (req, res) => {
+  const id = req.params.id
+  if (req.session.isLoggedIn==true && req.session.isAdmin==true) {
+    db.run("DELETE FROM projects WHERE pid=?", [id], function (error, theProjects){
+      if (error) {
+        const model = { dbError: true, theError: error,
+          isLoggedIn: req.session.isLoggedIn,
+          name: req.session.name,
+          isAdmin: req.session.isAdmin,
+        }
+        res.render("home.handlebars", model)
+      } else {
+        const model = { dbError: false, theError: "",
+          isLoggedIn: req.session.isLoggedIn,
+          name: req.session.name,
+          isAdmin: req.session.isAdmin,
+      }
+      res.render("home.handlebars", model)
+      } 
+    })
+  } else{
+    res.redirect('/login')
+  }
+})
+
+
 //-------------------------------
 //  LOG IN APP
 //-------------------------------
